fe4a939cf70de1170f1dd87e8624fbd1243a1f4a
Sean <>
2022-01-21 15:21:09 -0800

    setting up

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
-};
+
+const Question = () => <h2>question component</h2>;
 
 export default Question;
